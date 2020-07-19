e71e56a07322740c5f8edef66712d74374cd13aa
IsaiahT-Tech <>
2020-07-19 09:14:46 -0700

    Added query parameters

--- tutorial02/main.py
+++ tutorial02/main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 async def hello_world():
     return {'Hello' : 'World'}
 
+@app.get("/component/{component_id}") # path parameter
+async def get_component(component_id: int):
+    return {"component_id": component_id}
+
+@app.get("/component/")
+async def read_component(number: int, text: Optional[str]):  # query parameter
+    return {"number": number, "text": text}
+
+
