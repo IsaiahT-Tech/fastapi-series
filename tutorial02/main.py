593edaf07e5d9d20e1c744af441c8b064c75a35e
IsaiahT-Tech <>
2020-07-19 08:58:47 -0700

    Modified main.py

--- tutorial02/main.py
+++ tutorial02/main.py
@@ -1,9 +1,9 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get('/')
 async def hello_world():
-    return {"Hello": "World"}
+    return {'Hello' : 'World'}
 
