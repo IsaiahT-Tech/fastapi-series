13f1035d664aa95b1854dbfa8d7286427fb3431f
IsaiahT-Tech <>
2020-07-19 09:18:13 -0700

    Modifed repo

---
+++ tutorial03/main.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def hello_world():
+    return {'Hello' : 'World'}
+
+@app.get("/component/{component_id}") # path parameter
+async def get_component(component_id: int):
+    return {"component_id": component_id}
+
+@app.get("/component/")
+async def read_component(number: int, text: Optional[str]):  # query parameter
+    return {"number": number, "text": text}
+
+
