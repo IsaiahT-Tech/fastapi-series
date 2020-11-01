87d9e4dcb2bae5d8ffc0cd91f491517f437ddb0f
IsaiahT-Tech <63564966+IsaiahT-Tech@users.noreply.github.com>
2020-11-01 09:35:32 -0800

    Added Tutorial08

---
+++ tutorial08/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+@app.get("/book/{id}", response_class=HTMLResponse)
+async def read(request: Request, book: str, id: int):
+    return templates.TemplateResponse("index.html", {"request": request, "book": book, "id": id})
