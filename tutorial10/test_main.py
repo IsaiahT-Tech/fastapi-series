0dc3150aa569a5ec8142e9506eb670ba500860ee
IsaiahT-Tech <63564966+IsaiahT-Tech@users.noreply.github.com>
2021-03-08 06:51:47 -0800

    Modified repo

--- tutorial10/test_main.py
+++ tutorial10/test_main.py
@@ -1,12 +1,16 @@
 # Tutorial 10
-
+from fastapi import FastAPI
 from fastapi.testclient import TestClient 
 
-from main import app 
+app = FastAPI()
+
+@app.get('/')
+async def read_root():
+    return {"message": "Hello World"}
 
 client = TestClient(app)
 
-def test_create_todo():
-    response = client.post('/create/')
+def test_read_main():
+    response = client.get('/')
     assert response.status_code == 200
-
+    asser response.json() == {"message": "Hello World"}
--- tutorial11/test_main.py
+++ tutorial11/test_main.py
@@ -1,61 +1,48 @@
-from fastapi.testclient import TestClient      
+from fastapi.testclient import TestClient 
 
-from main import app
+from main import app 
 
 client = TestClient(app)
 
+data = {
+    "name": "IsaiahT-Tech",
+    "due_date": "Today",
+    "description": "string"
+}
+
 def test_create_todo():
-    response = client.post("/todo/",
-    json = {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
-    })
+    response = client.post("/todo/", json=data)
     assert response.status_code == 200
-    assert response.json() == {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
-    }
+    assert response.json() == data
 
 def test_get_all_todo():
-    data = {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
-    }
-    response = client.get("/todo/")
+    response = client.get("/todo/", json=data)
     assert response.status_code == 200
+    assert data in response.json()
 
 def test_get_todo():
-    data = {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
-    }
     response = client.get("/todo/0")
     assert response.status_code == 200
     assert response.json() == data
 
 def test_update_todo():
-    response = client.put("/todo/0",
-    json = {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
+    response = client.put("/todo/0", json = {
+        "name": "Test",
+        "due_date": "Now",
+        "description": "Python"
     })
     assert response.status_code == 200
-    assert response.json() ==  {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
+    assert response.json() == {   
+        "name": "Test",
+        "due_date": "Now",
+        "description": "Python"
     }
 
 def test_delete_todo():
     response = client.delete("/todo/0")
     assert response.status_code == 200
-    assert response.json() ==  {
-        "name": "string",
-        "due_date": "string",
-        "description": "string"
+    assert response.json() == {   
+        "name": "Test",
+        "due_date": "Now",
+        "description": "Python"
     }
