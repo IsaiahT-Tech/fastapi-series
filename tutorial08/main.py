bc75e4bec773af80292c548512a6f53be0c58288
IsaiahT-Tech <63564966+IsaiahT-Tech@users.noreply.github.com>
2021-03-15 16:10:00 -0700

    Update main.py

--- tutorial08/main.py
+++ tutorial08/main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/book/{id}", response_class=HTMLResponse)
 async def read(request: Request, book: str, id: int):
-    return templates.TemplateResponse("index.html", {"request": request, "book": book, "id": id})ush
+    return templates.TemplateResponse("index.html", {"request": request, "book": book, "id": id})
